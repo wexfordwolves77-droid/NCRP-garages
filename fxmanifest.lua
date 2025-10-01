d27fb6a08640a6660020477cca4ad7edb6177e1e
LouieAir <wexfordwolves77@gmail.com>
2025-09-30 22:20:10 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Louie'
+description 'NCRP ESX Garage System with 15+ Locations'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+server_script 'server.lua'
+client_script 'client.lua'
